4e376074d4e7948cddb12383d9fb629098db9114
imcuttle <moyuyc95@gmail.com>
2018-09-08 00:22:08 +0800

    chore: update readme

--- packages/util.createmount/index.d.ts
+++ packages/util.createmount/index.d.ts
@@ -1,16 +1,16 @@
-import { ReactElement } from 'react'
+import { ReactElement } from 'react';
 export interface IMountOptions<P = any> {
-  mountNode?: Node
-  attributes?: any
-  element?: ReactElement<P>
+    mountNode?: Node;
+    attributes?: any;
+    element?: ReactElement<P>;
 }
 export interface IMountCenter<P = any> {
-  close: () => void
-  open: (options?: IMountOptions<P>) => Node
+    close: () => void;
+    open: (options?: IMountOptions<P>) => Node;
 }
 /**
  *
  * @param {IMountOptions<P>} centerOpts
  * @return {IMountCenter}
  */
-export default function createMountCenter<P = any>(centerOpts?: IMountOptions<P>): IMountCenter
+export default function createMountCenter<P = any>(centerOpts?: IMountOptions<P>): IMountCenter;
