53414abcab6d7eb84d5983dd7b0cab0243975a4f
imcuttle <moyuyc95@gmail.com>
2018-09-07 23:24:39 +0800

    ci: update script

---
+++ scripts/filter.js
@@ -0,0 +1,32 @@
+/**
+ * @file filter
+ * @author Cuttle Cong
+ * @date 2018/9/7
+ *
+ */
+const globby = require('globby')
+const nps = require('path')
+const merge = require('lodash.merge')
+const fs = require('fs')
+;(async () => {
+  const cwd = nps.join(__dirname, '..')
+  const paths = await globby(require('../lerna').packages, {
+    cwd,
+    onlyDirectories: true
+  })
+
+  const packageJsonPaths = paths.map(path => nps.join(path, 'package.json'))
+
+  packageJsonPaths.forEach(path => {
+    path = nps.join(cwd, path)
+    const pkg = require(path)
+    const newPkg = merge({}, pkg, {
+      devDependencies: {
+        react: '^16.4.2'
+      }
+    })
+
+    fs.writeFileSync(path, JSON.stringify(newPkg, null, 2))
+  })
+  console.warn(paths)
+})()
